521890a4f5c3083c8c2a65edebe666b7a42b45bb
Unnat Singh <unnatsingh5@gmail.com>
2019-05-03 21:15:42 +0530

    Changing the view template

--- app/server.py
+++ app/server.py
@@ -60,9 +60,10 @@ async def analyze(request):
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    pred = re.findall('(.*?);',str(prediction)+';')
+    # pred = re.findall('(.*?);',str(prediction)+';')
     # print(prediction)
-    return JSONResponse({'result': dict_[pred[0]]})
+    pred = prediction.obj[-1]
+    return JSONResponse({'result': str(pred)})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
