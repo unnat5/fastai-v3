8acd10f79b822d783186147773e3de07d3b61a67
Unnat Singh <unnatsingh5@gmail.com>
2019-05-03 22:25:52 +0530

    Changing the view template

--- app/server.py
+++ app/server.py
@@ -64,7 +64,7 @@ async def analyze(request):
     # print(prediction)
     # pred = prediction[0].obj[-1]
     out = sorted(zip(learn.data.classes, map(float, prediction[1])),key=lambda p: p[1],reverse=True)
-    out = [dict_[i[0]] for i in sorted(zip(learn.data.classes, map(float, out[1])),key=lambda p: p[1],reverse=True) if i[1] == 1.0]
+    out_ = [dict_[i[0]] for i in out if i[1] == 1.0]
     return JSONResponse({'result': out })
 
 if __name__ == '__main__':
