02e2442ac2eb0e823440b03f9516aab88fdd83a2
Unnat Singh <unnatsingh5@gmail.com>
2019-05-03 22:32:14 +0530

    Changing the view template

--- app/server.py
+++ app/server.py
@@ -65,7 +65,7 @@ async def analyze(request):
     # pred = prediction[0].obj[-1]
     out = sorted(zip(learn.data.classes, map(float, prediction[1])),key=lambda p: p[1],reverse=True)
     out_ = [dict_[i[0]] for i in out if i[1] == 1.0]
-    return JSONResponse({'result': out })
+    return JSONResponse({'result': out_ })
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
